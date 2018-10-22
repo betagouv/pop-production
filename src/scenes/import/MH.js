82bf0f8b08fe5922634d85b109a62aaa801c0574
goffle <se.legoff@gmail.com>
2018-10-22 16:50:24 +0200

    add id creation

--- src/scenes/import/MH.js
+++ src/scenes/import/MH.js
@@ -4,6 +4,7 @@ import Importer from "./importer";
 import Merimee from "../../entities/merimee";
 import Palissy from "../../entities/palissy";
 
+import api from "../../services/api";
 import utils from "./utils";
 
 export default class Import extends React.Component {
@@ -27,16 +28,35 @@ function parseFiles(files, encoding) {
       reject("Pas de fichiers .csv detecté");
       return;
     }
-    utils.readCSV(objectFile, "|", encoding).then(objs => {
+    utils.readCSV(objectFile, "|", encoding).then(async objs => {
       const importedNotices = [];
       for (var i = 0; i < objs.length; i++) {
         const obj = objs[i];
+
         if (!obj.REF) {
           reject(
             "Impossible de détecter les notices. Vérifiez que le séparateur est bien | et que chaque notice possède une référence"
           );
           return;
         }
+
+        //Create New notices
+        if (obj.REF === "PM") {
+          if (!obj.DPT) {
+            reject("DPT est vide. Impossible de générer un id");
+            return;
+          }
+          const ref = await api.getNewId("palissy", "PM", obj.DPT);
+          obj.REF = ref.id;
+        } else if (obj.REF === "PA") {
+          if (!obj.DPT) {
+            reject("DPT est vide. Impossible de générer un id");
+            return;
+          }
+          const ref = await api.getNewId("merimee", "PA", obj.DPT);
+          obj.REF = ref.id;
+        }
+
         if (obj.REF.indexOf("PM") !== -1) {
           importedNotices.push(new Palissy(obj));
         } else if (obj.REF.indexOf("PA") !== -1) {

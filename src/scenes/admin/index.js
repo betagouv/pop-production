6a77d9ad84fb05922ecec61df04f5f24b40254a4
rap2h <raphaelht@gmail.com>
2018-12-04 07:00:11 +0100

    update users

--- src/scenes/admin/index.js
+++ src/scenes/admin/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Table } from "reactstrap";
 import CreateUser from "./createUser";
+import UpdateUser from "./UpdateUser";
 import { connect } from "react-redux";
 import api from "../../services/api";
 
@@ -12,51 +13,58 @@ class Admin extends React.Component {
     loading: true
   };
 
-  componentWillMount() {
+  fetchUsers = () => {
     this.setState({ loading: true });
     api.getUsers(this.props.group).then(users => {
       this.setState({ users: users || [], loading: false });
     });
+  };
+
+  componentWillMount() {
+    this.fetchUsers();
   }
 
   renderUsers() {
-    return <Table>
-      <thead>
-        <tr>
-          <th>Email</th>
-          <th>Nom</th>
-          <th>Prénom</th>
-          <th>Groupe</th>
-          <th>Institution</th>
-          <th>Role</th>
-        </tr>
-      </thead>
-      <tbody>
-        {this.state.users.map(user => {
-          const { email, prenom, nom, role, institution, group } = user;
-          return (
-            <tr key={email}>
-              <td>{email}</td>
-              <td>{nom}</td>
-              <td>{prenom}</td>
-              <td>{group}</td>
-              <td>{institution}</td>
-              <td>{role}</td>
-            </tr>
-          );
-        })}
-      </tbody>
+    return (
+      <Table>
+        <thead>
+          <tr>
+            <th>Email</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Groupe</th>
+            <th>Institution</th>
+            <th>Role</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.users.map(user => {
+            const { email, prenom, nom, role, institution, group } = user;
+            return (
+              <tr key={email}>
+                <td>{email}</td>
+                <td>{nom}</td>
+                <td>{prenom}</td>
+                <td>{group}</td>
+                <td>{institution}</td>
+                <td>{role}</td>
+                <td>
+                  <UpdateUser user={user} callback={this.fetchUsers} />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </Table>
-    ;
+    );
   }
 
   render() {
     return (
       <div className="admin">
         <CreateUser />
-        <div className="usersList">
-          {this.renderUsers()}
-        </div>
+        <div className="usersList">{this.renderUsers()}</div>
       </div>
     );
   }

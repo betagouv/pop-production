b7c3a5c07daac3edefb43b3ad5b0905925c4900d
rap2h <raphaelht@gmail.com>
2019-01-07 16:41:21 +0100

    remove

--- src/entities/Mnr.js
+++
@@ -1,7 +0,0 @@
-import Notice from "./Notice";
-
-export default class MNR extends Notice {
-  constructor(body) {
-    super(body, "mnr");
-  }
-}
--- src/entities/Palissy.js
+++
@@ -1,7 +0,0 @@
-import Notice from "./Notice";
-
-export default class Palissy extends Notice {
-  constructor(body) {
-    super(body, "palissy");
-  }
-}
